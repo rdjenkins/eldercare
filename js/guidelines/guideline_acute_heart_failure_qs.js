58ffdb41e21f27cb583b0ee7374972abbef461a2
rdjenkins <richarddeanjenkins@gmail.com>
2017-10-22 20:39:29 +0100

    Added acute heart failure. Fixed unicode bug.

---
+++ js/guidelines/guideline_acute_heart_failure_qs.js
@@ -0,0 +1,40 @@
+// A standalone guideline with a mobile-first design ethos.
+// Can be added in to any webpage or HTML5 app as a drop-in piece of Javascript code.
+// e.g. <script src="stroke.js"></src>
+// Assumes the presence of Bootstrap 4 and JQuery but will work fine without.
+
+// NOTE - THIS FILE IS GENERATED AUTOMATICALLY BY go-scales.py
+// USING THE org-mode ENCODED SOURCE FILE IN THIS FOLDER
+
+document.write("<div id='eldercare-guideline-wrapper'>");
+
+document.write('<h1>Acute Heart Failure</h1>');
+document.write(' <p>Quality standards adapted from NICE 2015</p>');
+document.write('<h2>1 Natriuretic peptide</h2>');
+document.write(' <p>Adults presenting to hospital with new suspected acute heart failure have a single measurement of natriuretic peptide.</p>');
+document.write('<h2>2 ECHO</h2>');
+document.write(' <p>Adults admitted to hospital with new suspected acute heart failure and raised natriuretic peptide levels have a transthoracic doppler 2D echocardiogram within 48 hours of admission.</p>');
+document.write('<h2>3 Heart failure team</h2>');
+document.write(' <p>Adults admitted to hospital with acute heart failure have input within 24 hours of admission from a dedicated specialist heart failure team.</p>');
+document.write('<h2>4 Beta-blocker</h2>');
+document.write(' <p>Adults with acute heart failure due to left ventricular systolic dysfunction are started on, or continue with, beta‑blocker treatment during their hospital admission.</p>');
+document.write('<h2>5 ACE inhibitors and aldosterone antagonist</h2>');
+document.write(' <p>Adults admitted to hospital with acute heart failure and reduced left ventricular ejection fraction are offered an angiotensin‑converting enzyme (ACE) inhibitor and an aldosterone antagonist.</p>');
+document.write('<h2>6 Follow-up</h2>');
+document.write(' <p>Adults with acute heart failure have a follow‑up clinical assessment by a member of the community- or hospital‑based specialist heart failure team within 2 weeks of hospital discharge.</p>');
+document.write('<h3>Source</h3>');
+document.write(' <p><a href="https://www.nice.org.uk/guidance/qs103/chapter/List-of-quality-statements">NICE Quality standard QS103 December 2015</a></p>');
+
+
+document.write("</div>");//  end of 'eldercare-guideline-wrapper'
+
+window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        console.log("Great! jQuery present");
+        // do something fancy here :-)
+    } else {
+        // jQuery is not loaded
+        console.log("No jQuery");
+    }
+}
